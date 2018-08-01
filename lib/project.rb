e38824fa6b7544a60c1ad9e17f403ff2eadaae62
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-08-01 15:29:36 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,27 @@
+class Project
+  
+  attr_accessor :title, :backers
+  
+  @@backers = []
+  
+  def initialize(title)
+    @title = title
+    @backers = backers
+  end
+  
+  def add_backer(backer)
+    # backer1 = Backer.new(backer)
+    # project1 = Project.new(title)
+   
+    # @@backers <<  backer1.Project 
+    @backers << backer
+        backer.backed_projects << self
+
+  end
+  
+#   drake = Artist.new("Drake", "rap")
+# hotline_bling = Song.new("Hotline Bling")
+ 
+# hotline_bling.artist = drake
+
+end
